cef5bc0172187908f5ff555a5b5ef40b1f749c16
justin <justinledford@gmail.com>
2018-05-10 21:20:37 -0500

    fix brake off, show reward in test env

--- test_env.py
+++ test_env.py
@@ -9,7 +9,6 @@ import matplotlib.animation as animation
 from controller import Controller
 
 VIEW_DECODED = False
-BRAKE_DAMPEN = 1
 
 def process_obs(obs):
     return np.expand_dims(imresize(obs, (64, 64)), axis=0) / 255.
@@ -27,15 +26,19 @@ t = 0
 a = None
 obs = None
 
+total_reward = 0
+
 if VIEW_DECODED:
     fig = plt.figure()
     im = plt.imshow(np.zeros((64, 64, 3)), animated=True)
 
     def update_fig(*args):
-        global t, a, env, obs, encoder, decoder, done
+        global t, a, env, obs, encoder, decoder, done, total_reward
 
         if done == True:
+            print('total reward: %d' % total_reward)
             t = 0
+            total_reward = 0
             obs = env.reset()
             env.render()
             obs = process_obs(obs)
@@ -44,7 +47,9 @@ if VIEW_DECODED:
         decoded = decoder.predict(np.array([z]))[0]
 
         a = controller.get_action(z)
+        a[2] = 0
         obs, reward, done, info = env.step(a)
+        total_reward += reward
         env.render()
         obs = process_obs(obs)
         t += 1
@@ -58,7 +63,9 @@ if VIEW_DECODED:
 else:
     while True:
         if done == True:
+            print('total reward: %d' % total_reward)
             t = 0
+            total_reward = 0
             obs = env.reset()
             env.render()
             obs = process_obs(obs)
@@ -67,8 +74,9 @@ else:
         decoded = decoder.predict(np.array([z]))[0]
 
         a = controller.get_action(z)
-        a *= np.array([1, 1, 1-BRAKE_DAMPEN])
+        a[2] = 0
         obs, reward, done, info = env.step(a)
+        total_reward += reward
         env.render()
         obs = process_obs(obs)
         t += 1
